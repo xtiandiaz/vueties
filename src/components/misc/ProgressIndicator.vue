ea504941937ff4d3025d60c16ad4d017d8366a24
Cristian Díaz <xtndiaz@gmail.com>
2025-04-08 18:03:32 -0500

    Add Progress Indicator component

---
+++ src/components/misc/ProgressIndicator.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Icon } from '@design-tokens/iconography'
+import SvgIcon from './SvgIcon.vue'
+</script>
+
+<template>
+  <SvgIcon :icon="Icon.Spinner" class="progress-indicator" />
+</template>
+
+<style scoped lang="scss">
+@use '@design-tokens/palette';
+
+@keyframes spin {
+  100% { rotate: 180deg; }
+}
+
+.progress-indicator {
+  animation: spin 1s linear 0s infinite;
+  display: block;
+  @include palette.color-attribute('color', 'accessory');
+}
+</style>
