e31cc2799d99ecaa00e3c6981e5ad446aecb6364
Cristian Díaz <xtndiaz@gmail.com>
2025-05-15 09:57:34 -0500

    Rename and refactor SimpleKeypad to move its button within the pad. Remove KeypadButton

---
+++ src/components/pads/KeyPad.vue
@@ -0,0 +1,72 @@
+<script lang="ts" setup>
+import SvgIcon from '../misc/SvgIcon.vue';
+import type { KeypadKeyVM } from '../view-models'
+
+defineProps<{
+  keyVMs: KeypadKeyVM[]
+}>()
+
+const emits = defineEmits<{
+  input: [value: number | string]
+}>()
+</script>
+
+<template>
+  <div class="pad keypad">
+    <button 
+      v-for="(vm, index) of keyVMs" 
+      :key="index" 
+      :disabled="!vm.isEnabled" 
+      :class="{ iconized: vm.icon !== undefined }"
+      @click="emits('input', vm.value)"
+    >
+      <SvgIcon v-if="vm.icon" :icon="vm.icon" />
+      <label v-else :class="{ dimmed: vm.isDimmed }">{{ vm.label }}</label>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/pads';
+@use '../styles/buttons';
+@use '@/assets/varties';
+@use '@design-tokens/palette';
+@use '@design-tokens/typography';
+
+.keypad {
+  button {
+    border-radius: 0.5em;
+    height: 3em;
+    vertical-align: middle;
+    width: 3em;
+    @include palette.color-attributes((
+      'background-color': varties.$key-background-color,
+      'color': varties.$key-color
+    ));
+
+    &.iconized:enabled {
+      @include palette.color-attributes((
+        'background-color': varties.$key-color,
+        'color': varties.$key-background-color
+      ));
+    }
+
+    label {
+      @extend h5;
+      
+      &.dimmed {
+        opacity: 25%;
+      }
+    }
+
+    &:disabled {
+      background-color: transparent;
+    }
+
+    .svg-icon {
+      height: 2em;
+      width: 2em;
+    }
+  }
+}
+</style>
