492877cbf4ae89b7ae8bc9ab1c79ddfc7ebe96ad
Cristian Díaz <xtndiaz@gmail.com>
2025-05-24 17:47:21 -0500

    Add accessories category and DataMark component

---
+++ src/components/accessories/DataMark.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Icon } from '@design-tokens/iconography';
+import SvgIcon from '../misc/SvgIcon.vue';
+
+defineProps<{
+  icon: Icon,
+  value: string | number
+}>()
+</script>
+
+<template>
+  <span class="data-mark">
+    <SvgIcon :icon="icon" />
+    <span>{{ String(value) }}</span>
+  </span>
+</template>
+
+<style scoped lang="scss">
+@use '@design-tokens/typography';
+
+span.data-mark {
+  display: inline-block;
+  
+  &, > * {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  
+  &.small {
+    @extend .caption;
+  }
+  
+  &.large {
+    @extend .h6;
+  }
+  
+  .svg-icon {
+    width: 1em;
+    margin-right: 0.25em;
+  }
+}
+</style>
--- src/components/misc/SvgIcon.vue
+++ src/components/misc/SvgIcon.vue
@@ -12,6 +12,7 @@ defineProps<{
 
 <style scoped lang="scss">
 .svg-icon {
+  aspect-ratio: 1;
   display: inline-block;
   min-height: 1em;
   min-width: 1em;
