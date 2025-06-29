76df9bb2f5d00e2c49537a4d8bfb8f9408d96605
Cristian Díaz <xtndiaz@gmail.com>
2025-06-28 21:14:35 -0500

    Add Vuety Scene

---
+++ src/framework/scenes/VuetyScene.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { type VuetyNavigationBarVM } from '../components/bars/view-models'
+import VuetyNavigationBar from '../components/bars/VuetyNavigationBar.vue'
+
+defineProps<{
+  navigationBarVM?: VuetyNavigationBarVM
+}>()
+</script>
+
+<template>
+  <VuetyNavigationBar v-if="navigationBarVM" :viewModel="navigationBarVM" />
+  
+  <RouterView />
+</template>
