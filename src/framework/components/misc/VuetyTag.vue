63c5ea5025b4c5afd4e4c3613392b6aefc75853b
Cristian Díaz <xtndiaz@gmail.com>
2025-08-01 15:51:39 +0300

    Refine tag padding

--- src/framework/components/misc/VuetyTag.vue
+++ src/framework/components/misc/VuetyTag.vue
@@ -23,41 +23,55 @@ defineProps<{
 .vuety-tag {
   align-items: center;
   display: inline-flex;
-  gap: 0.25em;
-  padding: 0.25em 0.75em;
+  gap: 0.25rem;
+  padding: 0.25rem 0.75rem;
   position: relative;
   
   &, .svg-icon {
     @include vs.color-attribute('color', vs.$tag-color);
   }
   
+  &.small {
+    font-size: 0.9rem;
+    gap: 0.2rem;
+    padding: 0.125rem 0.5rem;
+  }
+  
+  &.tiny {
+    font-size: 0.75rem;
+    gap: 0.15rem;
+    padding: 0 0.375rem;
+  }
+  
+  &.iconized {
+    padding-left: 0.5rem;
+    
+    &.small {
+      padding-left: 0.25rem;
+      
+      .svg-icon {
+        @include vs.size(1.25rem);
+      }
+    }
+    
+    &.tiny {
+      padding-left: 0.25rem;
+      
+      .svg-icon {
+        @include vs.size(1rem);
+      }
+    }
+  }
+  
   .background {
-    border-radius: 2em;
+    border-radius: 2rem;
     opacity: vs.$tag-background-opacity;
     @include vs.position(absolute, 0, 0, 0, 0);
     @include vs.color-attribute('background-color', vs.$tag-background-color);
   }
   
   .svg-icon {
-    @include vs.size(1.5em);
-  }
-  
-  &.iconized {
-    padding-left: 0.5em;
-    
-    &.small {
-      padding-left: 0.25em;
-    }
-  }
-  
-  &.small {
-    font-size: 0.9em;
-    padding: 0.125em 0.5em;
-  }
-  
-  &.tiny {
-    font-size: 0.75em;
-    padding: 0 0.5em;
+    @include vs.size(1.5rem);
   }
 }
 </style>
